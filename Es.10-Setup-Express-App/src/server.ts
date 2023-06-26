041c05dda1b2eea827f9e76d4bbed883d0626711
RobertoFanara <fanararoberto7@gmail.com>
2023-06-26 15:38:16 +0200

    esercizio 11 completato

--- Es.10-Setup-Express-App/src/server.ts
+++ Es.10-Setup-Express-App/src/server.ts
@@ -1,63 +1,90 @@
-import express from 'express'
-import morgan from 'morgan'
-import "express-async-errors"
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import Joi from "joi";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
 
 type Planet = {
-    id: number,
-    name: string,
-  };
+  id: number;
+  name: string;
+};
 
-  type Planets = Planet[];
+type Planets = Planet[];
 
-  let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+const planetSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  name: Joi.string().required(),
+});
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
 //gettin planets
 app.get("/api/planets/:id", (req, res) => {
-    const {id} = req.params;
-    const planet = planets.find(item => item.id === Number(id))
-    res.status(200).json(planet)
-})
+  const { id } = req.params;
+  const planet = planets.find((item) => item.id === Number(id));
+  res.status(200).json(planet);
+});
 
 //posting new planets
-app.post("/api/planets/", (req, res) => {
-    const { id, name } = req.body;
-    const newPlanet = { id, name }
-    planets = [...planets, newPlanet]
-    console.log(planets)
-    res.status(201).json({msg: "Il nuovo pianeta è stato importato"})
-})
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  const validateNewPlanet = planetSchema.validate(newPlanet);
+  if (validateNewPlanet.error) {
+    return res
+      .status(400)
+      .json({ msg: validateNewPlanet.error.details[0].message });
+  } else {
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "Il pianeta è stato creato" });
+  }
+  console.log(planets);
+});
 
 //reformatting a planets
-app.put('/api/planets/:id', (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    planets = planets.map((planet) => planet.id === Number(id) ? {...planet, name} : planet)
-    console.log(planets)
-    res.status(200).json({msg: 'Il pianeta è stato aggiornato'})
-})
+app.put("/api/planets/:id", (req, res) => {
+  const id = req.params.id;
+  const { name } = req.body;
+  const validateUpdatedPlanet = planetSchema.validate(id);
+  if (validateUpdatedPlanet.error) {
+    return res
+      .status(400)
+      .json({ msg: validateUpdatedPlanet.error.details[0].message });
+  } else {
+    planets = planets.map((planet) =>
+      planet.id === Number(id) ? { ...planet, name } : planet
+    );
+    res.status(200).json({ msg: "Il pianeta è stato aggiornato" });
+  }
+  console.log(planets);
+});
 
-//deleting a planet  
-app.delete('/api/planets/:id', (req, res) => {
-    const { id } = req.params;
-    planets = planets.filter(planet => planet.id !== Number(id));
-    console.log(planets)
-    res.status(200).json({msg: 'Il pianeta è stato cancellato'})
-})
+//deleting a planet
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((planet) => planet.id !== Number(id));
+  console.log(planets);
+  res.status(200).json({ msg: "Il pianeta è stato cancellato" });
+});
